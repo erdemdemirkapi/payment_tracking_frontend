3568a47bf0eac6dbf8b1772b7caf008feeb9094c
erdem <demirkapierdem@gmail.com>
2020-01-18 19:21:01 +0300

    redirection and transactions

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  Redirect
 } from "react-router-dom";
 import "./login.css";
 import "./signup.css";
-import './App.css';
-import axios from 'axios';
+import "./App.css";
+import axios from "axios";
 
-export default function BasicExample() {
+export default function App() {
   return (
     <Router>
       <div>
@@ -32,6 +33,9 @@ export default function BasicExample() {
           <Route path="/signup">
             <Signup />
           </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
         </Switch>
       </div>
     </Router>
@@ -41,6 +45,7 @@ export default function BasicExample() {
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   function validateForm() {
     return username.length > 0 && password.length > 0;
@@ -48,47 +53,54 @@ function Login() {
 
   function loginSubmit(event) {
     event.preventDefault();
-
     axios({
-      method: 'post',
-      url: 'http://127.0.0.1:8000/api/v1/rest-auth/login/',
+      method: "post",
+      url: "http://127.0.0.1:8000/api/v1/rest-auth/login/",
       data: {
-          username: username,
-          password: password,
+        username: username,
+        password: password
       }
     }).then(obj => {
-        console.log(obj.data);
-    })
+      if (obj.status === 200) {
+        console.log("succesful");
+        setIsLoggedIn(true);
+      } else {
+        console.log("error");
+      }
+    });
   }
 
   return (
-    <div>
-      <h2>Login</h2>
-      <div className="Login">
-        <form onSubmit={loginSubmit}>
-          <FormGroup controlId="username" bsSize="large">
-            <label>Username</label>
-            <FormControl
-              autoFocus
-              type="username"
-              value={username}
-              onChange={e => setUsername(e.target.value)}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" bsSize="large">
-            <label>Password</label>
-            <FormControl
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              type="password"
-            />
-          </FormGroup>
-          <Button block bsSize="large" disabled={!validateForm()} type="submit">
-            Login
-          </Button>
-        </form>
+    <React.Fragment>
+      {isLoggedIn && <Redirect to="/dashboard" />}
+      <div>
+        <h2>Login</h2>
+        <div className="Login">
+          <form onSubmit={loginSubmit}>
+            <FormGroup controlId="username">
+              <label>Username</label>
+              <FormControl
+                autoFocus
+                type="username"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup controlId="password">
+              <label>Password</label>
+              <FormControl
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                type="password"
+              />
+            </FormGroup>
+            <Button block disabled={!validateForm()} type="submit">
+              Login
+            </Button>
+          </form>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
 
@@ -99,17 +111,16 @@ function Signup() {
   const [password2, setPassword2] = useState("");
 
   function validateForm() {
-    var items = username.length > 0 && 
-            password1.length > 0 && 
-            password2.length > 0 && 
-            email.length > 0;
+    var items =
+      username.length > 0 &&
+      password1.length > 0 &&
+      password2.length > 0 &&
+      email.length > 0;
     var passwords = password1 === password2;
 
-    if(!items){
-
-    }else if(!passwords){
-
-    }else{
+    if (!items) {
+    } else if (!passwords) {
+    } else {
       return true;
     }
   }
@@ -118,65 +129,98 @@ function Signup() {
     event.preventDefault();
 
     axios({
-      method: 'post',
-      url: 'http://127.0.0.1:8000/api/v1/rest-auth/registration/',
+      method: "post",
+      url: "http://127.0.0.1:8000/api/v1/rest-auth/registration/",
       data: {
-          username: username,
-          email: email,
-          password1: password1,
-          password2: password2,
+        username: username,
+        email: email,
+        password1: password1,
+        password2: password2
       }
     }).then(obj => {
-        console.log(obj.data);
-    })
+      console.log(obj.data);
+    });
   }
 
   return (
     <div>
-    <h2>Signup</h2>
-
-    <div className="Signup">
-      <form onSubmit={signupSubmit}>
-        <FormGroup controlId="username" bsSize="large">
-          <label>Username</label>
-          <FormControl
-            autoFocus
-            type="username"
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="email" bsSize="large">
-          <label>Email</label>
-          <FormControl
-            autoFocus
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <label>Password</label>
-          <FormControl
-            value={password1}
-            onChange={e => setPassword1(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <label>Confirm Password</label>
-          <FormControl
-            value={password2}
-            onChange={e => setPassword2(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
-          Login
-        </Button>
-      </form>
+      <h2>Signup</h2>
+      <div className="Signup">
+        <form onSubmit={signupSubmit}>
+          <FormGroup controlId="username">
+            <label>Username</label>
+            <FormControl
+              autoFocus
+              type="username"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup controlId="email">
+            <label>Email</label>
+            <FormControl
+              autoFocus
+              type="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup controlId="password1">
+            <label>Password</label>
+            <FormControl
+              value={password1}
+              onChange={e => setPassword1(e.target.value)}
+              type="password"
+            />
+          </FormGroup>
+          <FormGroup controlId="password2">
+            <label>Confirm Password</label>
+            <FormControl
+              value={password2}
+              onChange={e => setPassword2(e.target.value)}
+              type="password"
+            />
+          </FormGroup>
+          <Button block disabled={!validateForm()} type="submit">
+            Login
+          </Button>
+        </form>
+      </div>
     </div>
-  </div>
   );
 }
 
+function Dashboard() {
+  const [data, setData] = useState(null);
+
+  function getItems() {
+    axios({
+      method: "get",
+      url: "http://127.0.0.1:8000/api/v1/transactions/",
+      params: {}
+    }).then(obj => {
+      console.log(obj.data);
+      setData(obj.data);
+    });
+  }
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      {!data && <Button block type="submit" onClick={getItems}>
+        Get Items
+      </Button>}
+      {data && <div>
+        <ul className="list-group list-group-flush">
+          {data.map(item => (
+            <li className="list-group-item" key={item.id}>
+              <label className="label">Id:</label> {item.id} |
+              <label className="label">User:</label> {item.user.username} |
+              <label className="label">Amount Cents:</label> {item.amount_cents}
+            </li>
+          ))}
+        </ul>
+      </div>}
+    </div>
+  );
+}
